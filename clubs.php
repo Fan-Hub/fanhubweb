19d929a282720cbb32844ed3cd9894fff9f55475
Dave <contact@fan-hub.com>
2018-08-01 09:52:09 +0100

    Initial website commit

---
+++ clubs.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta name="description" content="Fan Hub is a free app for sports fans that saves them money on travel and rewards them for their loyalty. Users can track how many miles they've travelled and see where they feature in a leaderboard compared to other fans."><!-- <<<<   Add a brief decription of your app -->
+
+<title>FanHub - Information for Clubs</title>
+
+<!----  User-Scalable = NO  ---->
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+<!----  CSS  ---->
+<link href="site/css/styles.css" rel="stylesheet" type="text/css" />
+
+<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+</head>
+
+<body onload="MM_preloadImages('site/images/header_background.jpg')">
+
+<div class="loading_screen"></div>
+
+<div class="inner_page">
+
+<div class="header">
+
+  <ul class="menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="clubs.php">Clubs</a></li>
+    <li><a target="_blank" href="https://medium.com/fan-hub">Blog</a></li>
+    <!-- <li><a href="/trains">Trains</a></li> -->
+  </ul>
+
+  <div class="inner_wrapper">
+  
+    <div class="iphone_hand"></div>
+    
+    <div class="header_right_column">
+    
+    <h1>Engage your fanbase and reward their loyalty</h1>
+    <h2>Scroll down for more details...</h2>
+    
+    <!-- <a href="#" class="appstore_button"></a> -->
+    
+    <!-- <a href="#" class="google_play_button"></a> -->
+    
+    </div>
+  
+  </div>
+
+</div>
+
+<div class="features_section">
+
+  <div class="inner_wrapper">
+  
+  <h3>FanHub is Perfect for Clubs</h3>
+  
+	<ul class="features_list clubs">
+
+    <li>
+      <div class="feature_icon location"><i class="fas fa-star"></i></div>
+      <p class="feature_title">Engage your fanbase and reward their loyalty</p>
+      <p>We've created Fan Hub as a tool that enables clubs to reward their fans' loyalty whilst providing an innovative activation opportunity for your sponsors and commercial partners to connect with your fans.</p>
+    </lI>
+
+
+    <lI>
+    <div class="feature_icon location"><i class="fas fa-code"></i></div>
+      <p class="feature_title">Our Tech, Your Opportunity</p>
+      <p>Create as many rewards (and milestones that trigger them) as you wish. We have arranged some default rewards with some of the world's leading brands and you are able to use these or create your own rewards with any partner you choose, relevant for your own fanbase.</p>
+    </lI>
+
+
+    <lI>
+    <div class="feature_icon location"><i class="fas fa-magic"></i></div>
+      <p class="feature_title">Our Tool, Your Imagination</p>
+      <p>We provide the mechanism for your imagination to craft exciting opportunities for your fans. This could include rewarding fans for arriving early in your stadium, accruing a number of miles travelling over the season or for appearing in your club's fan leaderboard. Rewards could also include unique experiences at your club - use our tool to deliver whatever rewards you dream up.</p>
+    </lI>
+
+
+    <lI>
+      <div class="feature_icon location"><i class="fas fa-comment"></i></div>
+      <p class="feature_title">Authentic Content</p>
+      <p>Fans crave recognition for their loyalty and by checking into the stadium at each match they can soar up the fan leaderboard. This tool enables you to recognise individuals for their commitment to their team on social media or perhaps on a match day.</p>
+    </lI>
+
+    <lI>
+      <div class="feature_icon location"><i class="fas fa-gift"></i></div>
+      <p class="feature_title">Added Value. New Revenue.</p>
+      <p>Brands demand better segmentation and authentic targeting...what could be more authentic than rewarding a fan for their loyal support of your team. Whether you include such an opportunity in your existing sponsorship packages or build a new revenue stream around this as a stand alone sponsorship opportunity, this is the ideal asset to compliment traditional mediums such as a pitch side board or match day sponsorship.</p>
+    </lI>
+
+
+    <lI>
+      <div class="feature_icon location"><i class="fas fa-train"></i></div>
+      <p class="feature_title">No fees, just cheap travel.</p>
+      <p>Saving the most compelling point for last, we charge no fees for Fan Hub and seek no commercial benefit from any additional revenues that you generate through our app. Our mission is to put fans first and one way in which we do that is to save fans money on their travel arrangements for every match. Our revenues are generated when fans save money rather than spending more!</p>
+    </lI>
+
+
+  
+  </ul>
+
+
+
+
+ </div> 
+</div>
+
+
+<div class="features_section" style="padding-top:20px; padding-bottom:40px;">
+  <div class="inner_wrapper">
+    <h3 style="margin-bottom:40px;">Watch Our Video</h3>
+    <div class="video" style="display:block;width:90%;max-width:600px; margin:0 auto;">
+    <iframe src="https://player.vimeo.com/video/276193404" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+  </div>
+</div>
+
+
+<div class="team_section">
+  <div class="inner_wrapper">
+    <h4>Some Of Our Partners</h4>
+    <div class="team_member_wrapper">
+
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_1.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Papa Johns</p>
+    
+    </div>
+    
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_2.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Amazon</p>
+    
+    </div>
+    
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_3.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">iTunes</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_4.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">UBER</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_5.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Caffe Nero</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_6.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Britannia Hotels</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_7.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Etoro</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_8.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Booking.com</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_9.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Raileasy</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_10.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">JustPark</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_11.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Gourmet Society</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_12.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">UK Soccer Shop</p>
+    
+    </div>
+
+ </div> 
+</div>
+
+
+<!-- <div class="final_cta_section">
+ <div class="inner_wrapper">
+  <h4>Download our app</h4>
+    <div class="final_cta_store_buttons_wrapper">
+      <a style="margin-left:auto;margin-right:auto;float:none;" href="#" class="appstore_button"></a>
+    </div>
+ </div> 
+
+</div> -->
+
+<!-- Signup Form -->
+
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://fan-hub.us17.list-manage.com/subscribe/post?u=6dfe35352d4ba1b955ee525dc&amp;id=bdf3482bb1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+  <label for="mce-EMAIL">We can email you when the app is released!</label>
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6dfe35352d4ba1b955ee525dc_bdf3482bb1" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+
+<div class="footer">
+  <div class="inner_wrapper">
+    <div class="footer_social_icons">
+      <a href="https://twitter.com/Fan_Hub2018" class="twitter_icon"></a>
+      <a href="https://www.facebook.com/fan.hub.351" class="fb_icon"></a>
+      <a href="https://www.instagram.com/fanhub2018/" class="ig_icon"></a>
+      <a href="#" class="appstore_icon"></a>
+    </div>
+     
+    <p>FanHub</p>
+    <p><a href="privacy.php">Privacy Policy</a></p>
+    <p class="copyright">© 2018</p>
+  </div>
+
+</div>
+
+</div>
+<!----  Scripts ---->
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<script src="site/js/site.js"></script>
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us17.list-manage.com","uuid":"6dfe35352d4ba1b955ee525dc","lid":"bdf3482bb1"}) })</script>
+
+<!----  Preload Images  ---->
+<script>
+function MM_preloadImages() { //v3.0
+var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+</script>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta name="description" content="Fan Hub is a free app for sports fans that saves them money on travel and rewards them for their loyalty. Users can track how many miles they've travelled and see where they feature in a leaderboard compared to other fans."><!-- <<<<   Add a brief decription of your app -->
+
+<title>FanHub - Fan Loyalty Rewarded</title>
+
+<!----  User-Scalable = NO  ---->
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+<!----  CSS  ---->
+<link href="site/css/styles.css" rel="stylesheet" type="text/css" />
+
+<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+</head>
+
+<body onload="MM_preloadImages('site/images/header_background.jpg')">
+
+<div class="loading_screen"></div>
+
+<div class="inner_page">
+
+<div class="header">
+
+  <ul class="menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="clubs.php">Clubs</a></li>
+    <li><a target="_blank" href="https://medium.com/fan-hub">Blog</a></li>
+    <!-- <li><a href="/trains">Trains</a></li> -->
+  </ul>
+
+  <div class="inner_wrapper">
+  
+    <div class="iphone_hand"></div>
+    
+    <div class="header_right_column">
+    
+    <h1>Fan Hub is a free app for fans that regularly watch live sport...</h1>
+    
+    <!-- <a href="#" class="appstore_button"></a> -->
+    
+    <!-- <a href="#" class="google_play_button"></a> -->
+    
+    </div>
+  
+  </div>
+
+</div>
+
+<div class="section_one">
+
+  <div class="inner_wrapper">
+  
+    <div class="section_one_header">
+    
+    <h2>Fan Loyalty Rewarded...</h2>
+    
+    <p>Use the Fan Hub app to track how far you travel over the season to watch your team, feature on the leaderboard of the most loyal fans, quickly find the best travel options and receive rewards from some of our partners.</p>
+    
+    </div>
+    
+    <div class="section_one_iphones_wrapper">
+    
+      <div class="section_one_iphone_one"></div>
+      
+      <div class="section_one_iphone_two"></div>
+      
+      <div class="section_one_iphone_three"></div>
+      
+    </div>
+    
+ </div> 
+ 
+</div>
+
+
+<div class="features_section">
+
+  <div class="inner_wrapper">
+  
+  <h3>Fan Hub Features That You'll Love...</h3>
+  
+	<ul class="features_list">
+
+    <lI>
+      <div class="feature_icon location"><i class="fas fa-map-marker"></i></div>
+      <p class="feature_title">Check In and Track</p>
+      <p>Check in at the stadium to track the miles that you travel over the season.</p>
+    </lI>
+
+
+    <lI>
+    <div class="feature_icon location"><i class="fas fa-trophy"></i></div>
+      <p class="feature_title">Be The Best Fan</p>
+      <p>See how your loyalty compares with other fans in each club's leaderboard.</p>
+    </lI>
+
+
+    <lI>
+    <div class="feature_icon location"><i class="fas fa-h-square"></i></div>
+      <p class="feature_title">Easy Away Days</p>
+      <p>Fan Hub automatically finds your best travel options for each fixture.</p>
+    </lI>
+
+
+    <lI>
+      <div class="feature_icon location"><i class="fas fa-gift"></i></div>
+      <p class="feature_title">Get Rewarded</p>
+      <p>Receive rewards from some of our partners in recognition of your loyalty.</p>
+    </lI>
+  
+  </ul>
+
+
+
+
+ </div> 
+</div>
+
+
+<div class="features_section" style="padding-top:20px; padding-bottom:40px;">
+  <div class="inner_wrapper">
+    <h3 style="margin-bottom:40px;">Watch Our Video</h3>
+    <div class="video" style="display:block;width:90%;max-width:600px; margin:0 auto;">
+    <iframe src="https://player.vimeo.com/video/276193404" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
+  </div>
+</div>
+
+
+<div class="team_section">
+  <div class="inner_wrapper">
+    <h4>Some Of Our Partners</h4>
+    <div class="team_member_wrapper">
+
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_1.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Papa Johns</p>
+    
+    </div>
+    
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_2.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Amazon</p>
+    
+    </div>
+    
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_3.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">iTunes</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_4.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">UBER</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_5.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Caffe Nero</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_6.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Britannia Hotels</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_7.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Etoro</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_8.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Booking.com</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_9.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Raileasy</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_10.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">JustPark</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_11.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">Gourmet Society</p>
+    
+    </div>
+
+    <div class="team_member_wrapper">
+      <div class="team_member_avatar">
+        <img src="site/images/team_member_12.png" width="180" height="120" alt=""/>
+      </div>
+      <p class="team_member_position">UK Soccer Shop</p>
+    
+    </div>
+
+ </div> 
+</div>
+
+<!-- Signup Form -->
+
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://fan-hub.us17.list-manage.com/subscribe/post?u=6dfe35352d4ba1b955ee525dc&amp;id=bdf3482bb1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+  <label for="mce-EMAIL">We can email you when the app is released!</label>
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6dfe35352d4ba1b955ee525dc_bdf3482bb1" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+<!-- / Signup Form -->
+
+<!-- <div class="final_cta_section">
+ <div class="inner_wrapper">
+  <h4>Download our app</h4>
+    <div class="final_cta_store_buttons_wrapper">
+      <a style="margin-left:auto;margin-right:auto;float:none;" href="#" class="appstore_button"></a>
+    </div>
+ </div> 
+
+</div> -->
+
+
+<div class="footer">
+  <div class="inner_wrapper">
+    <div class="footer_social_icons">
+      <a href="https://twitter.com/Fan_Hub2018" class="twitter_icon"></a>
+      <a href="https://www.facebook.com/fan.hub.351" class="fb_icon"></a>
+      <a href="https://www.instagram.com/fanhub2018/" class="ig_icon"></a>
+      <a href="#" class="appstore_icon"></a>
+    </div>
+     
+    <p>FanHub</p>
+    <p><a href="privacy.php">Privacy Policy</a></p>
+    <p class="copyright">© 2018</p>
+  </div>
+
+</div>
+
+</div>
+<!----  Scripts ---->
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<script src="site/js/site.js"></script>
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us17.list-manage.com","uuid":"6dfe35352d4ba1b955ee525dc","lid":"bdf3482bb1"}) })</script>
+
+<!----  Preload Images  ---->
+<script>
+function MM_preloadImages() { //v3.0
+var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+</script>
+
+
+</body>
+</html>
